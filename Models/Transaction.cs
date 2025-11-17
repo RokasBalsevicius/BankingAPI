40152fc0d163bdaaa2a9b172cfe694581343dbd3
Rokas <rok.balse@gmail.com>
2025-11-17 21:38:14 +0200

    Created BankingApi project structure for start

---
+++ Models/Transaction.cs
@@ -0,0 +1,9 @@
+namespace BankingApi.Models;
+
+public class Transaction
+{
+    public int Id { get; set; }
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+    public decimal Amount { get; set; }
+    public string Description { get; set; } = "";
+}
