4c51f062f0693a3e90a67f61a277092d6523ed7f
Rokas <rok.balse@gmail.com>
2025-11-18 22:11:54 +0200

    Cleanup of irrelevant files/folders

--- Data/BankingDatabase.cs
+++
@@ -1,8 +0,0 @@
-using BankingApi.Models;
-
-namespace BankingApi.Database;
-
-public class BankingDb
-{
-    public List<Customer> Customers { get; set; } = new();
-}
--- Models/Account.cs
+++
@@ -1,9 +0,0 @@
-namespace BankingApi.Models;
-
-public class Account
-{
-    public int Id { get; set; }
-    public string Type { get; set; } = "";
-    public decimal Balance { get; set; }
-    public List<Transaction> Transactions { get; set; } = new();
-}
